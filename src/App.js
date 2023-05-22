8e19226b39f97db5807aced567ae69d2a8c4e811
samaditya jatar <samadityaj@gmail.com>
2023-05-22 14:18:58 +0530

    Signup comp done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <>
-     <Navbar/> 
+     
+      <BrowserRouter>
+        <Navbar/>
+        <div className="container">
+          <Routes>
+          <Route exact path="/signup" key={Signup} element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+   
     </>
   );
 }
---
+++ src/context/ProfileState.js
@@ -0,0 +1,25 @@
+// import profileContext from "./ProfileContext";
+// import { useState } from "react";
+// import React from "react";
+
+// export const profileState = (props) => {
+//   const host = "http://localhost:5000";
+//   const profileInitial = [];
+//   const [profile, setprofile] = useState(profileInitial);
+
+  
+//   const addProfile = async (name , email , descripton , designation , company , address)=>{
+//     //API CAll
+    
+//   }
+//   const updateProfile = async (name , email , descripton , designation , company , address)=>{
+//     //API CAll
+//   }
+
+//   return (
+//     <profileContext.Provider value = {{profile , addProfile , updateProfile}}>
+//         {props.children}
+//     </profileContext.Provider>
+
+//   )
+// };
